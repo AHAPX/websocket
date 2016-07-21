8b515f5b948cb7b611648508ce005e16e36626a5
AHAPX <anarchy.b@gmail.com>
2016-07-21 03:03:49 -0400

    small refactoring

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
-from websocket import Client, WebSocketServer
+from websocket import Client, WebSocketServer, run
 
 
 class MockSocket:
@@ -17,25 +17,25 @@ class TestWebSocket(TestCase):
         self.assertTrue(Client(MockSocket(True)).is_active())
         client = Client(MockSocket(True))
         self.assertFalse(client.is_tag('tag1'))
-        self.assertEqual(client.name, '')
+        self.assertEqual(client.name, 'unknown')
         client.init_tags(['tag1', 'tag2'])
         self.assertTrue(client.is_tag('tag1'))
         self.assertEqual(client.name, 'tag1, tag2')
 
     def test_server(self):
-        server = WebSocketServer(lambda a: a, lambda a: a, 'localhost', 6379, 0, 'ws-channel')
+        server = WebSocketServer()
         # test run
         with patch('asyncio.async') as mock,\
                 patch('asyncio.base_events.BaseEventLoop.run_until_complete') as mock1,\
                 patch('asyncio.base_events.BaseEventLoop.run_forever') as mock2:
-            server.run('localhost', 9999)
+            run(MagicMock, MagicMock, 'localhost', 999, 'localhost', 6379, 0, 'ws-channel')
             mock.assert_called_once()
             mock1.assert_called_once()
             mock2.assert_called_once()
         # test receiver
         client = Client(MockSocket(True))
         server.receiver(client, 'msg')
-        self.assertEqual(client.name, '')
+        self.assertEqual(client.name, 'unknown')
         server.receiver(client, '{"message": "msg", "tags": ["tag1", "tag2"]}')
         server.receiver(client, 'msg')
         self.assertEqual(client.name, 'tag1, tag2')
--- websocket.py
+++ websocket.py
@@ -8,86 +8,73 @@ import argparse
 import configparser
 
 import websockets
-import asyncio_redis
+from subscriber import add_params, handlers
 
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+logger.addHandler(logging.StreamHandler())
 
 
 class Client():
-    _socket = None
-    _tags = []
+    socket = None
+    tags = []
 
     def __init__(self, socket):
-        self._socket = socket
+        self.socket = socket
 
     def send(self, message):
         if self.is_active():
             if not isinstance(message, str):
                 try:
                     message = json.dumps(message)
-                except:
+                except TypeError:
                     message = str(message)
-            yield from self._socket.send(message)
+            yield from self.socket.send(message)
 
     def init_tags(self, tags):
-        self._tags = tags
+        self.tags = tags
+        logger.info('set tags "{}"'.format(tags))
 
     def is_active(self):
-        return self._socket and self._socket.open
+        return self.socket and self.socket.open
 
     def is_tag(self, tag):
-        return tag in self._tags
+        return tag in self.tags
 
     @property
     def name(self):
-        return ', '.join(self._tags)
+        return ', '.join(self.tags) if self.tags else 'unknown'
 
 
 class Clients():
-    _clients = []
+    clients = []
 
     def add_client(self, client):
         if client.is_active():
-            self._clients.append(client)
+            self.clients.append(client)
+            logger.debug('new client connected')
 
     def send(self, message, tags=[]):
         if tags:
             clients = []
-            for client in self._clients:
+            for client in self.clients:
                 for tag in tags:
                     if client.is_tag(tag):
                         clients.append(client)
                         break
         else:
-            clients = self._clients
+            clients = self.clients
         for client in clients:
             if not client.is_active():
-                self._clients.remove(client)
+                self.clients.remove(client)
             yield from client.send(message)
 
 
 class WebSocketServer():
-    server_handler, broker_handler = None, None
 
-    def __init__(self, server_handler, broker_handler, redis_host, redis_port, redis_db, channel):
+    def __init__(self):
         self.clients = Clients()
-        self.server_handler = server_handler
-        self.broker_handler = broker_handler
-        self.channel = channel
-        self.redis_host = redis_host
-        self.redis_port = redis_port
-        self.redis_db = redis_db
-
-    def run(self, host, port):
-        start_server = websockets.serve(
-            partial(self.server_handler, self), host, port
-        )
-        logger.info('websocket server started')
-        asyncio.async(self.broker_handler(self))
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(start_server)
-        loop.run_forever()
 
     def receiver(self, client, message):
         try:
@@ -122,24 +109,28 @@ def server_handler(server, websocket, path):
             logger.error('\n'.join(traceback.format_tb(sys.exc_info()[2])))
 
 
-@asyncio.coroutine
-def redis_handler(server):
-    connection = yield from asyncio_redis.Connection.create(
-        host=server.redis_host,
-        port=server.redis_port,
-        db=server.redis_db
+def redis_receiver(message, server):
+    try:
+        msg = json.loads(message)
+    except ValueError:
+        logger.warning('"{}" is not valid json'.format(message))
+    else:
+        logger.info('send: {} - "{}"'.format(', '.join(msg.get('tags', ['ALL'])), msg['message']))
+        yield from server.clients.send(msg.get('message'), msg.get('tags', []))
+    yield
+
+
+def run(server_handler, redis_handler, host, port, rhost, rport, rdb, rchannel):
+    server = WebSocketServer()
+    start_server = websockets.serve(
+        partial(server_handler, server), host, port
     )
-    subscriber = yield from connection.start_subscribe()
-    yield from subscriber.subscribe([server.channel])
-    while True:
-        try:
-            pub = yield from subscriber.next_published()
-            message = json.loads(pub.value)
-            logger.info('send: {} - "{}"'.format(', '.join(message['tags']) if 'tags' in message else 'ALL', message['message']))
-            tags = message['tags'] if 'tags' in message else []
-            yield from server.clients.send(message['message'], tags)
-        except Exception as exc:
-            logger.error('\n'.join(traceback.format_tb(sys.exc_info()[2])))
+    logger.info('websocket server started')
+    asyncio.async(redis_handler(add_params(server)(redis_receiver),
+        host=rhost, port=rport, db=rdb, channel=rchannel))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_server)
+    loop.run_forever()
 
 
 if __name__ == '__main__':
@@ -148,10 +139,12 @@ if __name__ == '__main__':
     parser.add_argument('--config', '-c', type=str, help='config file')
     parser.add_argument('--host', '-h', type=str, help='host', default='localhost')
     parser.add_argument('--port', '-p', type=int, help='port', default=9999)
-    parser.add_argument('--redis-host', type=str, help='redis host', default='localhost')
-    parser.add_argument('--redis-port', type=int, help='redis port', default=6379)
-    parser.add_argument('--redis-db', type=int, help='redis db', default=0)
-    parser.add_argument('--channel', type=str, help='redis channel', default='ws-channel')
+    parser.add_argument('--rhost', type=str, help='redis host', default='localhost')
+    parser.add_argument('--rport', type=int, help='redis port', default=6379)
+    parser.add_argument('--rdb', type=int, help='redis db', default=0)
+    parser.add_argument('--rchannel', type=str, help='redis channel', default='ws-channel')
+    parser.add_argument('--debug', '-D', help='debug mode', action='store_true', default=False)
+
     args = parser.parse_args()
 
     settings = vars(args)
@@ -160,16 +153,20 @@ if __name__ == '__main__':
         config.read(args.config)
         settings['host'] = config.get('main', 'host', fallback=settings['host'])
         settings['port'] = config.getint('main', 'port', fallback=settings['port'])
-        settings['redis_host'] = config.get('redis', 'host', fallback=settings['redis_host'])
-        settings['redis_port'] = config.getint('redis', 'port', fallback=settings['redis_port'])
-        settings['redis_db'] = config.getint('redis', 'db', fallback=settings['redis_db'])
-        settings['channel'] = config.get('redis', 'channel', fallback=settings['channel'])
+        settings['debug'] = config.get('main', 'debug', fallback=settings['debug'])
+        settings['rhost'] = config.get('redis', 'host', fallback=settings['rhost'])
+        settings['rport'] = config.getint('redis', 'port', fallback=settings['rport'])
+        settings['rdb'] = config.getint('redis', 'db', fallback=settings['rdb'])
+        settings['channel'] = config.get('redis', 'channel', fallback=settings['rchannel'])
 
-    WebSocketServer(
-        server_handler=server_handler,
-        broker_handler=redis_handler,
-        redis_host=settings['redis_host'],
-        redis_port=settings['redis_port'],
-        redis_db=settings['redis_db'],
-        channel=settings['channel'],
-    ).run(settings['host'], settings['port'])
+    if settings['debug']:
+        logger.setLevel(logging.DEBUG)
+
+    run(server_handler=server_handler,
+        redis_handler=handlers.redis,
+        host=settings['host'],
+        port=settings['port'],
+        rhost=settings['rhost'],
+        rport=settings['rport'],
+        rdb=settings['rdb'],
+        rchannel=settings['rchannel'])
